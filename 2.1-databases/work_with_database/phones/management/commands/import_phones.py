c15d30fdb1ebcfcfe304758d9d9414480472ac8f
Nikita Borzov <nvborzov@gmail.com>
2022-11-30 15:56:05 +0500

    task models_list_displaying completed

--- 2.1-databases/work_with_database/phones/management/commands/import_phones.py
+++ 2.1-databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,17 +1,33 @@
 import csv
 
 from django.core.management.base import BaseCommand
+from django.template.defaultfilters import slugify
 from phones.models import Phone
 
 
 class Command(BaseCommand):
+    help = f'Imports phones from "phones.csv" or (if specified with -f) from other file.'
+
     def add_arguments(self, parser):
-        pass
+        parser.add_argument(
+            '-f',
+            '--file',
+            action='store',
+            default='phones.csv',
+            help='*.csv file contains phones'
+        )
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as file:
+        with open(options['file'], 'r') as file:
             phones = list(csv.DictReader(file, delimiter=';'))
 
         for phone in phones:
-            # TODO: Добавьте сохранение модели
-            pass
+            Phone.objects.create(
+                id=phone['id'],
+                name=phone['name'],
+                price=phone['price'],
+                image=phone['image'],
+                release_date=phone['release_date'],
+                lte_exists=phone['lte_exists'],
+                slug=slugify(phone['name'])
+            )
--- 2.1-databases/work_with_database/phones/models.py
+++ 2.1-databases/work_with_database/phones/models.py
@@ -2,5 +2,9 @@ from django.db import models
 
 
 class Phone(models.Model):
-    # TODO: Добавьте требуемые поля
-    pass
+    name = models.CharField(max_length=30)
+    price = models.DecimalField(max_digits=9, decimal_places=2)
+    image = models.CharField(max_length=200)
+    release_date = models.DateField()
+    lte_exists = models.BooleanField()
+    slug = models.SlugField(null=False, unique=True)
