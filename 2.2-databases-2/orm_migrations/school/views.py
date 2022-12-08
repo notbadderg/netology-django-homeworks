690ca58b5678449bc6641bc3c8f9ad37f2ded836
Nikita Borzov <nvborzov@gmail.com>
2022-12-08 17:54:20 +0500

    additional task completed

--- 2.2-databases-2/orm_migrations/school/views.py
+++ 2.2-databases-2/orm_migrations/school/views.py
@@ -7,7 +7,7 @@ from .models import Student
 def students_list(request):
     template = 'school/students_list.html'
     ordering = 'group'
-    students = Student.objects.all().order_by(ordering)
+    students = Student.objects.all().order_by(ordering).prefetch_related('teachers')
 
     context = {
         'students': students
