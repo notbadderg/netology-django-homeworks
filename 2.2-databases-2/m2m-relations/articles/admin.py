7114db8b64819c82040048c42ac669385148b77c
Nikita Borzov <nvborzov@gmail.com>
2022-12-09 15:14:16 +0500

    tasks completed (fixed validation)

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -15,11 +15,14 @@ class ArticleInlineFormset(BaseInlineFormSet):
                 quantity_of_main_scopes += 1
                 if data.get('DELETE'):
                     quantity_of_main_scopes -= 1
-
-        if quantity_of_main_scopes != 1:
-            raise ValidationError('После сохранения должен остаться только 1 основной раздел')
-
-        return super().clean()
+        if quantity_of_main_scopes == 1:
+            return super().clean()
+        elif quantity_of_main_scopes < 1:
+            raise ValidationError('Укажите основной раздел '
+                                  '(в результате сохранения статья останется без основного раздела)')
+        else:
+            raise ValidationError('Основным может быть только один раздел '
+                                  '(в результате сохранения у статьи станет больше одного раздела)')
 
 
 class ArticleInline(admin.TabularInline):
