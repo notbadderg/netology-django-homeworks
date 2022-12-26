8b9c983c7fb7ced01ea38a218c4e2f51c4ea2119
Nikita Borzov <nvborzov@gmail.com>
2022-12-27 00:58:03 +0500

    main tasks completed

--- 3.4-django-testing/django_testing/django_testing/settings.py
+++ 3.4-django-testing/django_testing/django_testing/settings.py
@@ -124,3 +124,23 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'TEST_REQUEST_DEFAULT_FORMAT': 'json',
+    # 'DEFAULT_AUTHENTICATION_CLASSES': [
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ],
+    # 'DEFAULT_THROTTLE_CLASSES': [
+    #     'rest_framework.throttling.UserRateThrottle',
+    #     'rest_framework.throttling.AnonRateThrottle',
+    # ],
+    # 'DEFAULT_THROTTLE_RATES': {
+    #     'user': '20/minute',
+    #     'anon': '10/minute',
+    # },
+    # 'DEFAULT_FILTER_BACKENDS': [
+    #     'django_filters.rest_framework.DjangoFilterBackend'
+    # ],
+}
--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -1,2 +1,120 @@
-def test_example():
-    assert False, "Just test example"
+import pytest
+
+from rest_framework.test import APIClient
+from model_bakery import baker
+
+from students.models import Course, Student
+
+API_V1_COURSES_URL = '/api/v1/courses'
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def course_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Course, *args, **kwargs)
+    return factory
+
+
+@pytest.fixture
+def student_factory():
+    def factory(*args, **kwargs):
+        return baker.make(Student, *args, **kwargs)
+    return factory
+
+
+@pytest.mark.django_db
+def test_get_course_single(client, course_factory):
+    courses = course_factory(_quantity=1)
+    eval_course_index = 0
+    eval_course_id = courses[eval_course_index].id
+
+    response = client.get(f'{API_V1_COURSES_URL}/{eval_course_id}/')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['name'] == courses[int(eval_course_index)].name
+
+
+@pytest.mark.django_db
+def test_get_courses_list(client, course_factory):
+    courses = course_factory(_quantity=100)
+
+    response = client.get(f'{API_V1_COURSES_URL}/')
+    data = response.json()
+
+    assert response.status_code == 200
+    for i, c in enumerate(data):
+        assert c['name'] == courses[i].name
+
+
+@pytest.mark.django_db
+def test_get_courses_list_filter_id(client, course_factory):
+    courses = course_factory(_quantity=100)
+    eval_course_index = 42
+    eval_course_id = courses[eval_course_index].id
+
+    response = client.get(f'{API_V1_COURSES_URL}/?id={eval_course_id}')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data[0]['name'] == courses[eval_course_index].name
+
+
+@pytest.mark.django_db
+def test_get_courses_list_filter_name(client, course_factory):
+    courses = course_factory(_quantity=100)
+    eval_course_index = 69
+    eval_course_name = courses[eval_course_index].name
+
+    response = client.get(f'{API_V1_COURSES_URL}/?name={eval_course_name}')
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data[0]['id'] == courses[eval_course_index].id
+
+
+@pytest.mark.django_db
+def test_post_course_create(client):
+    count = Course.objects.count()
+    course_test_name = 'test_course123'
+
+    response = client.post(f'{API_V1_COURSES_URL}/', data={'name': course_test_name})
+    data = response.json()
+
+    assert response.status_code == 201
+    assert Course.objects.count() == count + 1
+    assert data['name'] == course_test_name
+
+
+@pytest.mark.django_db
+def test_patch_course_update(client, course_factory):
+    courses = course_factory(_quantity=100)
+    eval_course_index = 32
+    eval_course_id = courses[eval_course_index].id
+    course_old_name = courses[eval_course_index].name
+    course_test_new_name = 'test_course123321'
+
+    response = client.patch(f'{API_V1_COURSES_URL}/{eval_course_id}/', data={'name': course_test_new_name})
+    data = response.json()
+
+    assert response.status_code == 200
+    assert data['name'] != course_old_name
+    assert data['name'] == course_test_new_name
+
+
+@pytest.mark.django_db
+def test_delete_course(client, course_factory):
+    courses = course_factory(_quantity=100)
+    eval_course_index = 99
+    eval_course_id = courses[eval_course_index].id
+
+    response_1 = client.delete(f'{API_V1_COURSES_URL}/{eval_course_id}/')
+    response_2 = client.get(f'{API_V1_COURSES_URL}/{eval_course_id}/')
+
+    assert response_1.status_code == 204
+    assert response_2.status_code == 404
